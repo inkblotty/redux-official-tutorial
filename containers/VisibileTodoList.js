6b9ff13e7f9bb51bf280d0174e7d7d5bbca14cf6
inkblotty <inkblot.and.blush@gmail.com>
2016-06-02 03:05:56 -0600

    finished VisibleTodoList container with comments

--- containers/VisibileTodoList.js
+++ containers/VisibileTodoList.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const  getVisibileTodos => (todos, filter) {
+const getVisibileTodos => (todos, filter) {
 	switch (filter) {
 		case 'SHOW_ALL':
 			return todos
@@ -11,9 +12,32 @@ const  getVisibileTodos => (todos, filter) {
 	}
 }
 
+// necessary for using Redux's connect()
+/* tells how to transform the Redux store state
+	into usable props
+*/
 const mapStateToProps = (state) => {
 	return {
 		todos: getVisibleTodos(state.todos, state.visibilityFilter)
 	}
 }
 
+// helpful with Redux's dispatch()
+// similar to mapStateToProps
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onTodoClick: (id) => {
+			dispatch(toggleTodo(id))
+		}
+	}
+}
+
+/* connect() generates container components
+	that wrap the mapping functions for state and
+	dispatch actions */
+const VisibleTodoList = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(TodoList)
+
+export default VisibleTodoList
